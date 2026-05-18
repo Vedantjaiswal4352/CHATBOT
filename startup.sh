581d1c96cf1fc1571a6ffe5a3582fee07326320e
Vedantjaiswal4352 <jaiswalvedant4352@gmail.com>
2026-05-18 16:34:21 +0530

    added

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 pip install -r requirements.txt
-python -m streamlit run streamlit_rag_frontend.py --server.port 8080 --server.address 0.0.0.0
+python -m streamlit run streamlit_rag_frontend.py --server.port 8080 --server.address 0.0.0.0 --server.enableCORS false --server.enableXsrfProtection false
